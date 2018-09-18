3fccd2eeceac129bce1ba24d02e4ac89ec583f5e
Matthew Garber <mgarber@contino.io>
2018-09-18 12:09:31 -0400

    removing region as IAM resources are global and converting role into a variable

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -1,13 +1,12 @@
 provider "google" {
   credentials = "${file("terraform-iam.json")}"
   project = "${var.project}"
-#  region = "${var.region}"
 }
 
 resource "google_project_iam_binding" "iam_setup" {
   count = "${length(var.users)}"
   project = "${var.project}"
-  role = "roles/container.viewer"
+  role = "${var.iam_role}"
   members = [
    "${element(var.users, count.index)}"
 ]
