b8a4264bbd51d53cba39544ea1410f0686de2c03
gpatmore <gregorypatmore@gpmule.home>
2018-09-24 13:43:12 -0400

    initial commit, script to configure gcloud

---
+++ setup/gke/gcloud-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# @DESCRIPTION script to configure and authenticate to given gcloud project
+# @USAGE ./gcloud-setup.sh <PROJECT_ID>
+
+PROJECT_ID=${1:-us-k8s-wokshop};
+
+gcloud config set project ${PROJECT_ID};
+gcloud set compute/region us-east1;
+gcloud set compute/zone us-east1-b;
