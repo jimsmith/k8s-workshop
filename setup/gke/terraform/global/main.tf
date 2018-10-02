498213c73da353ebe94b5c4eb14449c34ba0c2d5
Matthew Garber <mgarber441@gmail.com>
2018-10-01 22:08:01 -0400

    passing permissions variable to role resource

--- setup/gke/terraform/global/main.tf
+++ setup/gke/terraform/global/main.tf
@@ -6,8 +6,15 @@ provider "google" {
 resource "google_project_iam_binding" "iam_setup" {
   count = "${length(var.users)}"
   project = "${var.project}"
-  role = "${var.iam_role}"
+  role = "projects/${var.project}/roles/${google_project_iam_custom_role.k8s-wokshop-custom-role.role_id}"
   members = [
    "${element(var.users, count.index)}"
 ]
+}
+
+resource "google_project_iam_custom_role" "k8s-wokshop-custom-role" {
+ role_id = "k8s-wokshop-oct-3"
+ title = "k8s-workshop-contino"
+ permissions = ["${var.permissions}"]
+ project = "${var.project}"
 }
