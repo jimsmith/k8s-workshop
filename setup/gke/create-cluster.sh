fc0986bc337a739bcd774ab1ca8f37016e0c9eae
gpatmore <gregorypatmore@gpmule.local>
2018-10-03 15:41:13 -0400

    updating

--- setup/gke/create-cluster.sh
+++ setup/gke/create-cluster.sh
@@ -3,8 +3,10 @@
 # @USAGE ./create-cluster <CLUSTER_NAME>
 CLUSTER_NAME=${1:-workshop};
 
-gcloud compute clusters create ${CLUSTER_NAME} \
+gcloud container clusters create ${CLUSTER_NAME} \
+  --cluster-version "1.10.7-gke.2" \
   --num-nodes=3 \
   --enable-autoscaling \
   --min-nodes 3 \
-  --max-nodes 5;
+  --max-nodes 5 \
+  --enable-network-policy;
--- setup/gke/gcloud-setup.sh
+++ setup/gke/gcloud-setup.sh
@@ -5,5 +5,5 @@
 PROJECT_ID=${1:-us-k8s-wokshop};
 
 gcloud config set project ${PROJECT_ID};
-gcloud set compute/region us-east1;
-gcloud set compute/zone us-east1-b;
+gcloud config set compute/region us-east1;
+gcloud config set compute/zone us-east1-b;
